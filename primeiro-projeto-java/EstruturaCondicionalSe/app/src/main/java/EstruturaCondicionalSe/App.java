c0b257231e6ac788c223c78115cf0513b247689e
natancordeiro <o.natan1907@gmail.com>
2022-05-25 01:04:38 -0300

    commit

---
+++ primeiro-projeto-java/EstruturaCondicionalSe/app/src/main/java/EstruturaCondicionalSe/App.java
@@ -0,0 +1,42 @@
+package EstruturaCondicionalSe;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+
+        int media = 7;  
+        
+        if(media >= 7){
+            if(media == 0) {
+                //PARABÉNS! APROVADO COM NOTA MÁXIMA
+            } else {
+              //APROVADO
+            }
+        } else {
+            //REPROVADO 
+        }
+        
+        // ESTRUTURA ESCOLHA
+        
+        int cod = 2;
+
+        switch(cod) {
+            
+            case 1:
+                
+                break;
+                
+            case 2:
+                
+                break;
+            
+            default: 
+            
+        }
+        
+        
+    }
+}
---
+++ primeiro-projeto-java/VariaveisTipoOperadores/app/src/main/java/VariaveisTipoOperadores/App.java
@@ -0,0 +1,30 @@
+package VariaveisTipoOperadores;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        
+        // TIPOS PRIMITIVOS 
+        int idade = 0; // Variável que armazena números inteiros.
+        float cotacaoDolar = 5.0f; // Variável que armazena números reias (com vírgula)
+        double cotacaoEuro = 6.0d; 
+        char genero = 'M'; // Armazena apenas uma letra
+        byte placar = 0; 
+        boolean estaCadastrado = false;
+        
+        //TIPOS NÃO PRIMITIVOS MAIS COMUNS
+        String nome = "Esse é um texto de uma variável String";
+        
+        // OPERADORES ARITIMÉDICOS
+        
+        +
+        -
+        /        
+        *
+        %       
+                
+    }
+}
